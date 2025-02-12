933f898d5dbe0c91fffb7158af1426c4be76f899
Ashton Smith <ashton.smith@ubteam.com>
2025-02-13 08:17:40 +1300

    create initial functionality

--- src/Vusion.php
+++ src/Vusion.php
@@ -1,13 +1,48 @@
 <?php
 
+//initially a single file - to be split out later on
+
 namespace ubtashton\vusioncloudsdk;
+
+require_once 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
 
 class Vusion
 {
-    public function __construct()
-    {
+    public $apikey;
+    public $region;
+    public $baseurl = '';
 
+    public function __construct($apikey, $region= 'eu'){
+        $this->apikey = $apikey;
+        $this->region = $region;
+        $this->baseurl = 'https://api-'.$this->region.'.vusion.io/vusion-cloud/v1/';
     }
+
+
+    public function sendPost($url, $data){
+        $client = new Client();
+        $response = $client->request('POST', $this->baseurl.$url, [
+            'headers' => [
+                'Ocp-Apim-Subscription-Key' => $this->apikey,
+            ],'body' => json_encode($data)
+        ]);
+        return json_decode($response->getBody());
+    }
+
+
+    public function storeSearch($search){
+        $send=new \stdClass();
+        $send->search=($search!=null?$search:'');
+        $send->page=1;
+        $send->pageSize=500;
+        return $this->sendPost('stores/search', $send);
+    }
+
+
+
+
 
     
 }
